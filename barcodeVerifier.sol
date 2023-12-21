57bebb67e6cee68edcce56f6990c1ede47a8fec6
Emre K <110906681+kocaemre@users.noreply.github.com>
2023-12-21 16:00:45 +0300

    comments added

--- barcodeVerifier.sol
+++ barcodeVerifier.sol
@@ -3,35 +3,43 @@ pragma solidity ^0.8.0;
 
 contract FakeProductRegistry {
     address public owner;
+
+    // Mapping to keep track of registered products
     mapping(uint => bool) private productRegistry;
+
+    // Mapping to keep track of authorized sellers
     mapping(address => bool) private isSeller;
 
-
+    // Constructor sets the owner to the address that deploys the contract
     constructor() {
         owner = msg.sender;
     }
 
+    // Modifier to restrict access to only the owner or authorized sellers
     modifier Auth() {
-        require(msg.sender == owner || isSeller[msg.sender] == true,"Only authorized access");
+        require(msg.sender == owner || isSeller[msg.sender] == true, "Only authorized access");
         _;
     }
 
+    // Modifier to restrict access to only the owner
     modifier onlyOwner(){
-        require(msg.sender == owner,"Only owner access");
+        require(msg.sender == owner, "Only owner access");
         _;
     }
 
+    // Function to give access to a new seller, can only be called by the owner
     function giveAccess(address seller) external onlyOwner {
-        require(!isSeller[seller],"Seller already registered");
+        require(!isSeller[seller], "Seller already registered");
         isSeller[seller] = true;
     }
 
-    
+    // Function to register a new product, can be called by the owner or authorized sellers
     function registerProduct(uint barcode) external Auth {
         require(!productRegistry[barcode], "Product already registered");
         productRegistry[barcode] = true;
     }
 
+    // Function to verify if a product is registered
     function verifyProduct(uint barcode) external view returns (bool) {
         return productRegistry[barcode];
     }
